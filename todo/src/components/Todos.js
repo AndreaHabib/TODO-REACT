d87b773ac0c5c6fccea2f90b9ee2b1792720581d
Andrea Habib <andrea.atef@yahoo.com>
2020-06-02 18:03:21 -0400

    added items and cleaned up folder

---
+++ todo/src/components/Todos.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import TodoItem from './todoItem';
+import PropTypes from 'prop-types';
+class Todos extends Component {
+  render() {
+    return this.props.todos.map((todo) =>(
+        <TodoItem key={todo.id} todo={todo} isComplete={this.props.isComplete}/>
+    ));
+ }
+}
+Todos.propTypes = {
+    todos: PropTypes.array.isRequired
+}
+export default Todos;
